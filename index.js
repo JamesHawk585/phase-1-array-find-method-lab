6d85e0c2a8956a1b15e2b36cba164afc1584ca73
James Hawk <jph94880@gmail.com>
2023-04-09 12:17:31 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "N/A"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //{ year: "2012", result: "W"}
+  ]
+
+//const recordCopy = Array.from(record);
+
+//recordCopy.shift()
+
+function superbowlWin(array) {
+    for (const outcome of array) {
+        if (outcome.result === "W") {
+            return outcome.year;
+        } 
+    }
+
+} 
+
+
+console.log(superbowlWin(record))
+//console.log(superbowlWin(recordCopy))
+
+//console.log(recordCopy)
+ 
+ 
+ 
+ 
+ 
